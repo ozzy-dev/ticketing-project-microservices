eaf7433bea2ff6b5848defef03f7e2b3aed0bc01
ozzy-dev <80585830+ozzy-dev@users.noreply.github.com>
2022-06-29 10:10:50 -0400

    Project

---
+++ app-domain-model/src/main/java/com/cydeo/dto/UserRequestDTO.java
@@ -0,0 +1,14 @@
+package com.cydeo.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRequestDTO {
+
+    public Long userId;
+    public String username;
+
+}
