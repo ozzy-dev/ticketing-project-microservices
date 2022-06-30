46d96f0fbb1469686db48f01f67123529278332f
ozzy-dev <80585830+ozzy-dev@users.noreply.github.com>
2022-06-30 15:24:21 -0400

    Discovery Service

--- app-client-management/src/main/java/com/cydeo/service/UserClientService.java
+++ app-client-management/src/main/java/com/cydeo/service/UserClientService.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "user-service",url = "http://localhost:9090")
+@FeignClient(value = "user-service",url = "http://user-service:9090")
 public interface UserClientService {
 
     @GetMapping("/api/v1/user/{username}")
