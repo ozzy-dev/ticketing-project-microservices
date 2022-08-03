cf9e52d92e0e5f6de51fd91501b1453c46ac88e1
ozzy-dev <80585830+ozzy-dev@users.noreply.github.com>
2022-08-03 10:36:34 -0400

    SECURITY

--- gateway-server/src/main/java/com/cydeo/config/SecurityConfig.java
+++ gateway-server/src/main/java/com/cydeo/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.jwt.Jwt;
