a2158d8548f989dbadad54b586ada2618a34231e
ozzy-dev <80585830+ozzy-dev@users.noreply.github.com>
2022-08-04 12:41:48 -0400

    Revert "OpenApi"
    This reverts commit 00d7bcc8ed7ca1fcd0d2366f98c8d98c04558f11.

--- task-service/src/main/java/com/cydeo/TaskServiceApplication.java
+++ task-service/src/main/java/com/cydeo/TaskServiceApplication.java
@@ -1,7 +1,5 @@
 package com.cydeo;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,9 +11,6 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 @EnableFeignClients
 @EnableDiscoveryClient
-@OpenAPIDefinition(info =
-@Info(title = "Ticketing App", version = "${springdoc.version}", description = "Task Service API v1.0")
-)
 public class TaskServiceApplication {
 
     public static void main(String[] args) {
